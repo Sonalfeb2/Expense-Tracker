d6a3b68fe09af8d0500f3d35aa9206a37501c142
Sonal Agrawal <sonalfeb7@gmail.com>
2023-07-10 14:54:15 +0530

    CRUD Done

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 let arr = [];
+var table = document.getElementById("table");
+var submit = document.getElementById("submit");
 if (localStorage.getItem("Expense")) {
   let data = JSON.parse(localStorage.getItem("Expense"));
-  var table = document.getElementById("table");
+
   var tbody = document.createElement("tbody");
   table.appendChild(tbody);
 
@@ -10,9 +12,16 @@ if (localStorage.getItem("Expense")) {
 
     var tr = document.createElement("tr");
     tbody.appendChild(tr);
+
     var td = document.createElement("td");
-    td.appendChild(document.createTextNode(value.amount));
+    td.appendChild(document.createTextNode(value.id));
     tr.appendChild(td);
+
+    td.className = "time";
+    var td1 = document.createElement("td");
+
+    td1.appendChild(document.createTextNode(value.amount));
+    tr.appendChild(td1);
     var td2 = document.createElement("td");
     td2.appendChild(document.createTextNode(value.des));
     tr.appendChild(td2);
@@ -39,11 +48,55 @@ var cat = document.getElementById("sel1");
 form.addEventListener("submit", getValues);
 function getValues(e) {
   e.preventDefault();
-  if (amount.value !== "" && des.value !== "" && cat.value !== "") {
-    arr.push({ amount: amount.value, des: des.value, cat: cat.value });
-    localStorage.setItem("Expense", JSON.stringify(arr));
+  if (!submit.getAttribute('key')) {
+    if (amount.value !== "" && des.value !== "" && cat.value !== "") {
+      const date = new Date();
+      arr.push({
+        id: date,
+        amount: amount.value,
+        des: des.value,
+        cat: cat.value
+      });
+      localStorage.setItem("Expense", JSON.stringify(arr));
+      window.location.reload();
+    } else {
+      alert("Please fill all inputs");
+    }
+  }
+  else{
+    var key = submit.getAttribute('key')
+    var localData = JSON.parse(localStorage.getItem("Expense"));
+    localData.forEach((value)=>{
+        if(value.id==key){
+            value.amount = amount.value;
+            value.des = des.value;
+            value.cat  = cat.value;
+        }
+    })
+    localStorage.setItem('Expense',JSON.stringify(localData));
     window.location.reload();
-  } else {
-    alert("Please fill all inputs");
+    
+  }
+}
+table.addEventListener("click", RemoveItem);
+function RemoveItem(e) {
+  if (e.target.classList.contains("delete")) {
+    if (confirm("Are You Sure")) {
+      var row = e.target.parentElement.parentElement;
+      var td = row.firstElementChild;
+      var localData = JSON.parse(localStorage.getItem("Expense"));
+      var newStore = localData.filter(value => value.date !== td.textContent);
+      localStorage.clear("Expense");
+      localStorage.setItem("Expense", JSON.stringify(newStore));
+      window.location.reload();
+    }
+  }
+  if (e.target.classList.contains("edit")) {
+    if (confirm("Are You Sure")) {
+      var row = e.target.parentElement.parentElement;
+      var td = row.firstElementChild;
+      submit.value = "Update";
+      submit.setAttribute("key", td.textContent);
+    }
   }
 }
